c9b2d15d7dc0551723cbec08e938718dbda78186
LeoXtr3m <leoncio.rios21@gmail.com>
2021-07-06 20:01:58 -0500

    intentando descargar archivos

--- app.js
+++ app.js
@@ -3,30 +3,35 @@ const app = express();
 
 const path = require('path');
 
-app.use(express.static('public'));
+
+
+app.use(express.static('public'));//Indica que usara la carpeta Public como estatica 
+app.set('view engine', 'ejs'); // Par indicar que se usara las herramientas de ejs 
+
+
 
 app.listen(3000, () => console.log('Server running in 3000 port'));
 
 app.get('/',(req,res) => {
-    res.sendFile(path.resolve('./views/index.html'));
+    res.sendFile(path.resolve('./views/products/index.ejs'));
 });
 
 app.get('/productos',(req,res) => {
-    res.sendFile(path.resolve('./views/productos.html'));
+    res.sendFile(path.resolve('./views/products/productos.ejs'));
 });
 
 app.get('/productDetail',(req,res) => {
-    res.sendFile(path.resolve('./views/productDetail.html'));
+    res.sendFile(path.resolve('./views/products/productDetail.ejs'));
 });
 
 app.get('/productCart',(req,res) => {
-    res.sendFile(path.resolve('./views/productCart.html'));
+    res.sendFile(path.resolve('./views/products/productCart.ejs'));
 });
 
 app.get('/register',(req,res) => {
-    res.sendFile(path.resolve('./views/register.html'));
+    res.sendFile(path.resolve('./views/users/register.ejs'));
 });
 
 app.get('/login',(req,res) => {
-    res.sendFile(path.resolve('./views/login.html'));
+    res.sendFile(path.resolve('./views/users/login.ejs'));
 });
