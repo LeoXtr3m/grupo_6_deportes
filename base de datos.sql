62108c572c6394d286ff40ec851acb1d12860517
LeoXtr3m <leoncio.rios21@gmail.com>
2021-09-27 01:27:39 -0500

    se agrega el documento de la base de datos para probar el sistema

---
+++ base de datos.sql
@@ -0,0 +1,25 @@
+##// PRODUCTOS
+ SELECT * FROM productos;
+#INSERT INTO Categorias(nombre, imagen) VALUES 
+#('Beisbol', 'beisbol.jpgcantidadcategory_idcategory_id'); 
+
+#SELECT * FROM usuarios;
+#INSERT INTO Usuarios(firstName, lastName, email, age, numberCel) VALUES 
+#('Leoncio', 'Rios','leoncio.rios21@gmail.com', '24', '6564654333'); 
+
+##// SECCION CATEGORIAS BUSCAR Y AGREGAR FILAS 
+##SELECT * FROM categorias;
+##INSERT INTO Categorias(nombre, imagen) VALUES 
+## ('Beisbol', 'beisbol.jpgcantidadcategory_idcategory_id'); 
+
+ #//carrito de compras productos
+#SELECT * FROM carrito;
+   #INSERT INTO carrito(usuario_id, producto_id, fecha, cantidad, total, comprado) VALUES 
+   #('5', '8', '2019-09-27', '1', '10', false); 
+ # delete from carrito where id = 4
+
+
+
+##/////ELIMINAR UNA CATEGORIA 
+#SELECT * FROM categorias;
+#delete from categorias where id = 3
